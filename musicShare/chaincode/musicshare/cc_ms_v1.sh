4fec6577d7d96d34c1950ff1e3d9066e9912cb2d
Chokoty <chokokty0816@gmail.com>
2021-11-26 20:27:51 +0900

    fixed docker-compose-template

--- musicShare/chaincode/musicshare/cc_ms_v1.sh
+++ musicShare/chaincode/musicshare/cc_ms_v1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-	echo "Arguments are missing. ex) ./cc_ms.sh instantiate 1.0.0"
-	exit 1
-fi
+# if [ $# -ne 2 ]; then
+# 	echo "Arguments are missing. ex) ./cc_ms.sh instantiate 1.0.0"
+# 	exit 1
+# fi
 
 instruction=$1
 version=$2
@@ -15,10 +15,10 @@ docker exec cli peer chaincode install -n musicshare -v $version -p github.com/m
 #chaincode instatiate
 docker exec cli peer chaincode $instruction -n musicshare -v $version -C mychannel -c '{"Args":[]}' -P 'OR ("Org1MSP.member", "Org2MSP.member","Org3MSP.member")'
 sleep 5
-#chaincode invoke user1
-docker exec cli peer chaincode invoke -n musicshare -C mychannel -c '{"Args":["addUser","user2"]}'
+#chaincode invoke music1
+docker exec cli peer chaincode invoke -n musicshare -C mychannel -c '{"Args":["register","Into the Night","YOASOBI","4:23","100"]}'
 sleep 5
-#chaincode query user1
+#chaincode query music1
 docker exec cli peer chaincode query -n musicshare -C mychannel -c '{"Args":["readRating","user2"]}'
 
 #chaincode invoke add rating
